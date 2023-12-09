cbbc151c075ed9cfb60598b6922d829c5f2750d7
Fabian Rudolf <84329481+fabian-rudolf@users.noreply.github.com>
2023-12-09 18:27:26 +0100

    Create pyspark_utils.py

---
+++ pyspark_utils.py
@@ -0,0 +1,41 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as fn
+from pyspark.sql.window import Window
+from typing import Optional
+
+def check_key_duplicates(spark: SparkSession, table_name: str, key_columns: list, limit: Optional[int] = 10):
+    """
+    Function to check the amount of key duplicates over columns in a table.
+    
+    Args:
+    spark (SparkSession): Spark Session.
+    table_name (str): Name of the table.
+    key_columns (list): List of key columns.
+    limit (int, optional): Limit for the output DataFrame if not `None`, defaults to 10.
+    
+    Returns:
+    DataFrame: DataFrame with the amount and percentage of duplicates in data.
+    """
+    # Define window
+    window = Window.partitionBy(*key_columns).orderBy(fn.lit(1))
+    
+    # Load table
+    df = spark.table(table_name)
+    
+    # Calculate key duplicates
+    df = df.withColumn('key_duplicate', fn.row_number().over(window))
+    
+    # Filter duplicates
+    df = df.filter(fn.col('key_duplicate') > 1)
+    
+    # Group by key columns and calculate max duplicates
+    df = df.groupBy(*key_columns).agg(fn.max('key_duplicate').alias('key_duplicates'))
+    
+    # Order by key duplicates
+    df = df.orderBy(fn.desc('key_duplicates'))
+    
+    # Limit output
+    if limit:
+      df = df.limit(limit)
+    
+    return df
